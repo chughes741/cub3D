095d85bc72453a5e22f2cae045ea47e512d59833
Cole Hughes - 42 <coltenhughes@gmail.com>
2023-01-24 12:15:46 -0500

    Fixed movement collision

--- src/moves.c
+++ src/moves.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 16:29:38 by chughes           #+#    #+#             */
-/*   Updated: 2023/01/23 15:14:50 by chughes          ###   ########.fr       */
+/*   Updated: 2023/01/24 12:14:37 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ void	move_east(t_data **data)
 	t_data	*d;
 
 	d = (*data);
-	if (!d->map[(int)(d->pos[X] + d->dir[X] * MOVE_SPEED)][(int)(d->pos[Y])])
+	if (!d->map[(int)(d->pos[X] + d->dir[Y] * MOVE_SPEED)][(int)(d->pos[Y])])
 		d->pos[X] += d->dir[Y] * MOVE_SPEED;
-	if (!d->map[(int)(d->pos[X])][(int)(d->pos[Y] + d->dir[Y] * MOVE_SPEED)])
+	if (!d->map[(int)(d->pos[X])][(int)(d->pos[Y] - d->dir[X] * MOVE_SPEED)])
 		d->pos[Y] -= d->dir[X] * MOVE_SPEED;
 	return ;
 }
@@ -57,9 +57,9 @@ void	move_west(t_data **data)
 	t_data	*d;
 
 	d = (*data);
-	if (!d->map[(int)(d->pos[X] - d->dir[X] * MOVE_SPEED)][(int)(d->pos[Y])])
+	if (!d->map[(int)(d->pos[X] - d->dir[Y] * MOVE_SPEED)][(int)(d->pos[Y])])
 		d->pos[X] -= d->dir[Y] * MOVE_SPEED;
-	if (!d->map[(int)(d->pos[X])][(int)(d->pos[Y] - d->dir[Y] * MOVE_SPEED)])
+	if (!d->map[(int)(d->pos[X])][(int)(d->pos[Y] + d->dir[X] * MOVE_SPEED)])
 		d->pos[Y] += d->dir[X] * MOVE_SPEED;
 	return ;
 }
