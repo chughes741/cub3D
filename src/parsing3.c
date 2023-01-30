e75022b33fc9acc4c40aa6dde8661b0b1d07a2ab
Cole Hughes - 42 <coltenhughes@gmail.com>
2023-01-30 14:33:54 -0500

    Fixed spaces in texture names

--- src/parsing3.c
+++ src/parsing3.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 15:39:41 by chughes           #+#    #+#             */
-/*   Updated: 2023/01/30 12:36:35 by chughes          ###   ########.fr       */
+/*   Updated: 2023/01/30 14:33:31 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@
 void	set_texture(int *i, int side)
 {
 	t_data	*data;
+	int		index;
 
 	data = get_data();
 	if (data->tex_path[side])
 		exit_error("too many texture paths in map file");
 	data->tex_path[side] = strpop(ft_strdup(data->map_file[*i] + 3), -1);
+	index = 0;
+	while (data->tex_path[side][0] == ' ')
+		data->tex_path[side] = strpop(data->tex_path[side], 0);
 	data->map_file = array_del_one(data->map_file, *i);
 	--(*i);
 }
